ccad304249edf88920f782e5c646011666c30cd6
HannahLange <82364625+HannahLange@users.noreply.github.com>
2022-12-16 21:57:56 -0500

    First version for fermions
    seems to work only for half filling, t=0.

---
+++ 2DFermions/model.py
@@ -0,0 +1,244 @@
+import torch
+from torch import nn
+import numpy as np
+
+
+class TensorizedGRU(nn.Module):
+    """ Custom GRU layer for 2D input """
+    def __init__(self, input_size, hidden_size, device):
+        super().__init__()
+        
+        self.input_size  = input_size
+        self.hidden_size = hidden_size
+        self.sigmoid = torch.nn.Sigmoid()
+        self.tanh    = torch.nn.Tanh()
+        self.device  = device
+        
+          
+        # define all weights
+        w1      = torch.empty(self.hidden_size, 2*self.hidden_size, 2*self.input_size)
+        self.W1 = nn.Parameter(w1).to(self.device)  # nn.Parameter is a Tensor that's a module parameter.
+        b1      = torch.empty(self.hidden_size)
+        self.b1 = nn.Parameter(b1).to(self.device)
+        
+        w2      = torch.empty(self.hidden_size, 2*self.hidden_size, 2*self.input_size)
+        self.W2 = nn.Parameter(w2).to(self.device) 
+        b2      = torch.empty(self.hidden_size)
+        self.b2 = nn.Parameter(b2).to(self.device)
+        
+        w3      = torch.empty(2*self.hidden_size, self.hidden_size)
+        self.W3 = nn.Parameter(w3).to(self.device) 
+
+        self.reset_parameters()
+        
+    def reset_parameters(self):
+        nn.init.xavier_uniform_(self.W1, 1)
+        nn.init.xavier_uniform_(self.W2, 1)
+        nn.init.xavier_uniform_(self.W3, 1)
+        fan_in, fan_out = nn.init._calculate_fan_in_and_fan_out(self.W1)
+        lim = np.sqrt(3.0 / (0.5*(fan_in+fan_out)))
+        nn.init.uniform_(self.b1, -lim, lim)
+        fan_in, fan_out = nn.init._calculate_fan_in_and_fan_out(self.W2) 
+        lim = np.sqrt(3.0 / (0.5*(fan_in+fan_out)))
+        nn.init.uniform_(self.b2, -lim, lim)
+   
+
+    def forward(self, inputs, states):
+        if len(inputs[0].size()) == 3:
+            inputs[0] = inputs[0][:,0,:]
+        if len(inputs[1].size()) == 3:
+            inputs[1] = inputs[1][:,0,:]
+
+        inputstate_mul = torch.einsum('ij,ik->ijk', torch.concat((states[0], states[1]), 1),torch.concat((inputs[0], inputs[1]),1))
+        # prepare input linear combination
+        state_mul1 = torch.einsum('ijk,ljk->il', inputstate_mul, self.W1) # [batch_sz, num_units]
+        state_mul2 = torch.einsum('ijk,ljk->il', inputstate_mul, self.W2) # [batch_sz, num_units]
+
+        u = self.sigmoid(state_mul2 + self.b2)
+        state_tilda = self.tanh(state_mul1 + self.b1) 
+
+        new_state = u*state_tilda 
+        new_state += (1.-u)*torch.einsum('ij,jk->ik', torch.concat((states[0], states[1]), 1), self.W3)
+        output = new_state
+        return output, new_state
+
+
+
+class Model(nn.Module):
+    def __init__(self, input_size, system_size_x, system_size_y, hidden_dim, n_layers, device):
+        super(Model, self).__init__()
+        """
+        Creates RNN consisting of GRU cells.
+        Inputs:
+            - input_size:  number of quantum numbers (i.e. 2 for spin-1/2 particles)
+            - system_size: length of each snapshot
+            - hidden_dim:  dimension of hidden states
+            - n_layers:    number of layers of the GRU
+        """
+
+        # Defining some parameters
+        self.input_size  = input_size    # number of expected features in input data
+        self.output_size = input_size    # number of expected features in output data
+        self.N_x         = system_size_x # length of generated samples in x dir
+        self.N_y         = system_size_y # length of generated samples in x dir
+        self.hidden_dim  = hidden_dim    # number of features in the hidden state
+        self.n_layers    = n_layers      # number of stacked GRUs
+        self.device      = device 
+        self.system_size = system_size_x*system_size_y
+        #Defining the layers
+        self.rnn  = TensorizedGRU(self.input_size, hidden_dim, self.device)   
+        self.lin1 = nn.Linear(hidden_dim, self.output_size)
+        self.lin2 = nn.Linear(hidden_dim, self.output_size)
+        #self.s    = torch.softmax(dim=0)
+        self.soft = nn.Softsign()
+        
+        self.get_num_parameters()
+        
+    def forward(self, x, hidden):
+        """
+        Passes the input through the network.
+        Inputs:
+            - x:      input state at t
+            - hidden: hidden state at t
+        Outputs:
+            - out:    output configuration at t+1
+            - hidden: hidden state at t+1
+        """
+        
+        # Passing in the input and hidden state into the model and obtaining outputs
+        out, hidden = self.rnn(x, hidden)
+        
+        # Reshaping the outputs such that it can be fit into the dense layer
+        out = out.contiguous().view(-1, self.hidden_dim)
+        
+        return out, hidden
+    
+    def init_hidden(self, batch_size):
+        """
+        Generates the hidden state for a given batch size.
+        """
+        # This method generates the first hidden state of zeros for the forward pass and passes it to the device.
+        # This is equivalent to a product state.
+        hidden = torch.zeros((batch_size, self.hidden_dim), dtype=torch.float64).to(self.device)
+        return hidden
+    
+    def get_num_parameters(self):
+        """
+        Calculates the number of parameters of the network. """
+        p = 0
+        for param in list(self.parameters()):
+            if param.requires_grad:
+                p += param.numel()
+        print("Total number of parameters in the network: "+str(p))
+        return p
+    
+    def _gen_samples(self, nx, ny, direction, inputs, hidden_inputs, numsamples):
+        # pass the hidden unit and sigma into the GRU cell at t=i 
+        # and get the output y (will be used for calculating the 
+        # probability) and the next hidden state
+        full_sigma = [inputs[str(nx+direction[0])+str(ny)],inputs[str(nx)+str(ny+direction[1])]]
+        hidden     = [hidden_inputs[str(nx+direction[0])+str(ny)],hidden_inputs[str(nx)+str(ny+direction[1])]]
+        y, hidden  = self.forward(full_sigma, hidden)
+        # the amplitude is given by a linear layer with a softmax activation
+        ampl = self.lin1(y)
+        ampl = torch.softmax(ampl,dim=1) # amplitude, all elements in a row sum to 1
+        # samples are obtained by sampling from the amplitudes
+        sample = torch.multinomial(ampl, 1) 
+        # one hot encode the current sigma to pass it into the GRU at
+        # the next time step
+        sigma = nn.functional.one_hot(sample, self.input_size).double()
+        
+        return sample[:,0], sigma, hidden
+    
+    
+    def sample(self, num_samples):
+        """
+        Generates num_samples samples from the network and returns the samples,
+        their log probabilities and phases.
+        """
+        # generate a first input of zeros (sigma and hidden states) to the first GRU cell at t=0
+        sigma       = torch.zeros((num_samples,self.input_size), dtype=torch.float64).to(self.device)
+        inputs = {}
+        hidden_inputs = {}
+        for ny in range(-1, self.N_y): # add a padding for the inputs and hidden states
+            for nx in range(-1, self.N_x+1):
+                inputs[str(nx)+str(ny)] = sigma
+                hidden_inputs[str(nx)+str(ny)] = self.init_hidden(num_samples)
+        
+        samples     = [[[] for ny in range(self.N_y)] for nx in range(self.N_x)]
+        for ny in range(self.N_y):
+            if ny % 2 == 0: #go from left to right
+                for nx in range(self.N_x):
+                    direction = [-1,-1]
+                    samples[nx][ny], sigma, hidden_inputs[str(nx)+str(ny)] = self._gen_samples(nx, ny, direction, inputs, hidden_inputs, num_samples)
+                    inputs[str(nx)+str(ny)] = sigma
+            else: #go from right to left
+                for nx in range(self.N_x-1, -1, -1):
+                    direction = [1,-1]
+                    samples[nx][ny], sigma, hidden_inputs[str(nx)+str(ny)] = self._gen_samples(nx, ny, direction, inputs, hidden_inputs, num_samples)
+                    inputs[str(nx)+str(ny)] = sigma
+                    
+        samples = torch.stack([torch.stack(s, axis=1) for s in samples], axis=1).to(self.device)  #.reshape((num_samples, self.N_x, self.N_y))
+        return samples
+    
+    def _gen_probs(self, nx, ny, direction, sample, inputs, hidden_inputs):
+        # pass the hidden unit and sigma into the GRU cell at t=i 
+        # and get the output y (will be used for calculating the 
+        # probability) and the next hidden state
+        full_sigma = [inputs[str(nx+direction[0])+str(ny)],inputs[str(nx)+str(ny+direction[1])]]
+        hidden     = [hidden_inputs[str(nx+direction[0])+str(ny)],hidden_inputs[str(nx)+str(ny+direction[1])]]
+        y, hidden  = self.forward(full_sigma, hidden)
+        # the amplitude is given by a linear layer with a softmax activation
+        ampl = self.lin1(y)
+        ampl = torch.softmax(ampl,dim=1) # amplitude, all elements in a row sum to 1
+        # the phase is given by a linear layer with a softsign activation
+        phase = self.lin2(y)
+        phase = self.soft(phase) 
+        # one hot encode the current sigma to pass it into the GRU at
+        # the next time step
+        sigma = nn.functional.one_hot(sample.reshape((sample.size()[0],1)), self.input_size).double()
+        
+        return sigma, ampl, torch.mul(torch.pi,phase), hidden
+    
+    def log_probabilities(self, samples):
+        """
+        Calculates the log probability and the phase of each item in samples.
+        """
+        # reshape samples
+        samples = samples.to(self.device)
+        num_samples = samples.size()[0]
+        samples = samples.clone().detach()
+        
+        # generate a first input of zeros (sigma and hidden states) to the first GRU cell at t=0
+        sigma  = torch.zeros((num_samples, self.input_size), dtype=torch.float64).to(self.device)
+        inputs = {}
+        hidden_inputs = {}
+        for ny in range(-1, self.N_y):
+            for nx in range(-1, self.N_x+1):
+                inputs[str(nx)+str(ny)] = sigma
+                hidden_inputs[str(nx)+str(ny)] = self.init_hidden(num_samples)
+
+        ampl_probs  = [[[] for ny in range(self.N_y)] for nx in range(self.N_x)]
+        phase_probs = [[[] for ny in range(self.N_y)] for nx in range(self.N_x)]
+        ohs         = [[[] for ny in range(self.N_y)] for nx in range(self.N_x)]
+        for ny in range(self.N_y):
+            if ny % 2 == 0: #go from left to right
+                for nx in range(self.N_x):
+                    direction = [-1,-1]
+                    sigma, ampl_probs[nx][ny], phase_probs[nx][ny], hidden_inputs[str(nx)+str(ny)] = self._gen_probs(nx, ny, direction, samples[:,nx,ny], inputs, hidden_inputs)
+                    ohs[nx][ny] = sigma
+                    inputs[str(nx)+str(ny)] = sigma
+            else: #go from right to left
+                for nx in range(self.N_x-1, -1, -1):
+                    direction = [1,-1]
+                    sigma, ampl_probs[nx][ny], phase_probs[nx][ny], hidden_inputs[str(nx)+str(ny)] = self._gen_probs(nx, ny, direction, samples[:,nx,ny], inputs, hidden_inputs)
+                    ohs[nx][ny] = sigma
+                    inputs[str(nx)+str(ny)] = sigma
+        ampl_probs = torch.cat([torch.stack(a, axis=1) for a in ampl_probs], axis=1) #.reshape((num_samples, self.N_x*self.N_y, 2))
+        phase_probs = torch.cat([torch.stack(p, axis=1) for p in phase_probs], axis=1) #.reshape((num_samples, self.N_x*self.N_y, 2))
+        ohs = torch.cat([torch.cat(o, axis=1) for o in ohs], axis=1) #.reshape((num_samples, self.N_x*self.N_y, 2))
+        # calculate the wavefunction and split it into amplitude and phase
+        log_probs_ampl = torch.sum(torch.log(torch.sum(torch.torch.multiply(ampl_probs,ohs), axis =2)), axis=1)
+        phase = torch.sum((torch.sum(torch.torch.multiply(phase_probs,ohs), axis =2)), axis=1)
+        return log_probs_ampl, phase
+        
---
+++ 2DFermions/run.py
@@ -0,0 +1,129 @@
+import torch
+from torch import nn
+import numpy as np
+import numpy.random as random
+import math
+import os
+import timeit 
+
+from model import Model
+from localenergy import tJ2D_Eloc
+import observables as o
+
+def save(model, boundaries, folder, device):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    torch.save(model.state_dict(), folder+"model_params.pt")
+    # calculate the nearest neighbor spin correlators
+    samples = model.sample(1000)
+    log_probs, phases = model.log_probabilities(samples)
+    szsz = np.array(o.get_szsz(samples, log_probs, boundaries, model, device))
+    np.save(folder+"szsz.npy", szsz)
+    sxsx = np.array(o.get_sxsx(samples, log_probs, phases, boundaries, model, device))
+    np.save(folder+"sxsx.npy", sxsx)
+    sysy = np.array(o.get_sysy(samples, log_probs, phases, boundaries, model, device))
+    np.save(folder+"sysy.npy", sysy)
+    
+def cost_fct(samples, model, J, t, boundaries, sz_tot, N_tot, N, symmetry=None):
+    if symmetry != None:
+        Eloc, log_probs, phases, sym_samples, sym_log_probs, sym_phases = tJ2D_Eloc(J,t,samples, model, boundaries, symmetry)
+        sym_log_psi = (0.5*sym_log_probs+1j*sym_phases)
+    else:
+        Eloc, log_probs, phases = tJ2D_Eloc(J, t, samples, model, boundaries, symmetry)
+    eloc_sum = (Eloc).mean(axis=0)
+    e_loc_corr = (Eloc - eloc_sum).detach()
+    log_psi = (0.5*log_probs+1j*phases)
+    if sz_tot != None:
+        e_loc_corr += (o.get_sz_(samples, model, device).detach()-sz_tot*torch.ones((samples.size()[0])))**2 
+    if N_tot != None:
+        e_loc_corr += (N/N_tot-torch.ones((samples.size()[0])))**2
+    cost = 2 * torch.real((torch.conj(log_psi) * e_loc_corr.to(torch.complex128))).mean(axis=0)
+    if symmetry != None:
+        cost += 2 * torch.abs(torch.real((torch.conj(log_psi)-(torch.conj(sym_log_psi))))).mean(axis=0)
+    return Eloc, cost, log_probs, phases
+
+
+
+# ---------- initial settings --------------
+# torch.cuda.is_available() checks and returns a Boolean True if a GPU is available, else it'll return False
+is_cuda = torch.cuda.is_available()
+
+# If we have a GPU available, we'll set our device to GPU. We'll use this device variable later in our code.
+if is_cuda:
+    device = torch.device("cuda")
+    print("GPU is available")
+else:
+    device = torch.device("cpu")
+    print("GPU not available, CPU used")
+random.seed(1234)
+np.random.seed(0)
+torch.manual_seed(1234)
+
+
+# --------- Define and initialize the model ------------
+
+# Define model parameters
+J          = 1
+t          = 0
+density    = 0.75
+Nx         = 4
+Ny         = 4
+bounds     = "open"
+load_model = False
+sz_tot     = 0
+N_tot      = int(Nx*Ny*density)
+
+# Define hyperparameters
+n_samples   = 400
+sym         = None
+n_epochs    = 1000
+max_grad    = 10
+lr          = 0.001
+lr_decay    = 200
+lr_thresh   = 0.0005
+hiddendim   = min(int(Nx*Ny/2),20)
+
+fol = str(Nx)+"x"+str(Ny)+"_qubits/J="+str(float(J))+"t="+str(float(t))+"den"+str(density)+"/"
+model = Model(input_size=3, system_size_x=Nx,system_size_y=Ny, hidden_dim=hiddendim, n_layers=1, device = device)
+model = model.to(device)
+model = model.double()
+if load_model:
+    if os.path.exists(fol+"model_params.pt"):
+        model.load_state_dict(torch.load(fol+"model_params.pt"))    
+
+# -------- Optimizer and cost function ------------------
+optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+    
+# --------- start the training ----------------------
+print("n")
+for epoch in range(1, n_epochs + 1):
+    start = timeit.default_timer()
+    optimizer.param_groups[0]['lr'] = max(lr_thresh, lr/(1+epoch/lr_decay))
+    samples = model.sample(n_samples)
+    optimizer.zero_grad() # Clears existing gradients from previous epoch
+    N  = o.get_particle_no(samples, model, device).mean(axis=0).detach()
+    Elocs, cost, log_probs, phases = cost_fct(samples, model, J, t, bounds, sz_tot, N_tot, N, symmetry=sym)
+    if max_grad != None:
+        if torch.abs(cost) > max_grad:
+            cost = cost/torch.abs(cost)*max_grad
+    cost.backward() # Does backpropagation and calculates gradients
+    optimizer.step() # Updates the weights accordingly
+    optimizer.zero_grad()
+    if (epoch >= 1/3*n_epochs) and sym == None:
+        print("Use spatial symmetry from now.")
+        sym = "C4"
+    Eloc = Elocs.mean().detach()
+    Eloc_var = (Elocs).var(axis=0) 
+    end = timeit.default_timer()
+    if epoch%10 == 0 or epoch == 1:
+        sx = o.get_sx(samples, log_probs, phases, model, device)
+        sy = o.get_sy(samples, log_probs, phases, model, device)
+        sz = o.get_sz(samples, model, device)
+        print('Epoch: {}/ {}/ t/epoch={}.............'.format(epoch, n_epochs, round(end-start,2)), end=' ')
+        print("Loss: {:.8f}".format(cost)+", mean(E): {:.8f}".format(Eloc)+", var(E): {:.8f}".format(Eloc_var)+", Sx: {:.4f}".format(sx)+", Sy: {:.4f}".format(sy)+", Sz: {:.4f}".format(sz)+", N/N_tot: {:.4f} %".format(N.numpy()/N_tot))
+    if epoch != n_epochs: del cost, samples, log_probs, phases, Eloc
+# ----------- save -----------------------------------
+save(model, bounds, fol, device)
+np.save(fol+"/Eloc.npy", np.array(Eloc))
+
