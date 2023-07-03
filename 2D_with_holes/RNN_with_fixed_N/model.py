7b5d3c6e6166b9480c20511d6d9f1b1784f00227
HannahLange <82364625+HannahLange@users.noreply.github.com>
2023-07-03 15:48:22 +0200

    default type: double

--- 2D_with_holes/RNN_with_fixed_N/model.py
+++ 2D_with_holes/RNN_with_fixed_N/model.py
@@ -2,12 +2,14 @@ import torch
 from torch import nn
 import numpy as np
 import timeit
+from torch import index_select
 
-torch.set_default_tensor_type(torch.FloatTensor)
+
+torch.set_default_tensor_type(torch.DoubleTensor)
 torch.manual_seed(1234)
 
 class TensorizedRNN(nn.Module):
-    def __init__(self, input_size, hidden_size, device, celltype="GRU"):
+    def __init__(self, input_size, hidden_size, device, dtype=torch.float64, celltype="GRU"):
         super().__init__()
         """ 
         Custom RNN / GRU layer for 2D input 
@@ -16,6 +18,7 @@ class TensorizedRNN(nn.Module):
             device: CPU or GPU
             celltype: plain vanilla "RNN" or gated unit "GRU"
         """
+
         self.input_size  = input_size
         self.hidden_size = hidden_size
         self.sigmoid  = torch.nn.Sigmoid()
@@ -27,7 +30,7 @@ class TensorizedRNN(nn.Module):
 
         # define all layers / weights
         input_dim = 2*self.input_size
-        factory_kwargs = {'device': device, 'dtype': torch.float32}
+        factory_kwargs = {'device': device, 'dtype': dtype}
         if self.celltype == "GRU" or self.celltype == "RNN":
             w1      = torch.empty((input_dim, 2*self.hidden_size, self.hidden_size),**factory_kwargs)
             self.W1 = nn.Parameter(w1)  # nn.Parameter is a Tensor that's a module parameter.
@@ -99,7 +102,7 @@ class TensorizedRNN(nn.Module):
 
 
 class Model(nn.Module):
-    def __init__(self, input_size, system_size_x, system_size_y, N_target, sz_total, hidden_dim, weight_sharing, device):
+    def __init__(self, input_size, system_size_x, system_size_y, N_target, sz_total, hidden_dim, weight_sharing, device, dtype=torch.float64):
         super(Model, self).__init__()
         """
         Creates RNN consisting of GRU cells.
@@ -125,12 +128,13 @@ class Model(nn.Module):
         self.rnn_type    = "Non-Dilated" # else: Dilated 
         self.system_size = system_size_x*system_size_y
         self.weight_sharing = weight_sharing
+        self.dtype       = dtype
         #Defining the layers
         if self.weight_sharing == True:
             print("Use RNN cell with weight sharing.")
             self.rnn  = TensorizedRNN(self.input_size, hidden_dim, self.device)   
-            self.lin1 = nn.Linear(hidden_dim, self.output_size, device=self.device, dtype=torch.float32)
-            self.lin2 = nn.Linear(hidden_dim, self.output_size, device=self.device, dtype=torch.float32)
+            self.lin1 = nn.Linear(hidden_dim, self.output_size, device=self.device, dtype=self.dtype)
+            self.lin2 = nn.Linear(hidden_dim, self.output_size, device=self.device, dtype=self.dtype)
         elif self.weight_sharing == False or self.rnn_type == "Dilated":
             self.rnn = []
             self.lin1 = []
@@ -146,8 +150,8 @@ class Model(nn.Module):
                 for i in range(self.N_x):
                     for j in range(self.N_y):
                         if nl == 0:
-                            lin1.append(nn.Linear(hidden_dim, self.output_size, device=self.device, dtype=torch.float32))
-                            lin2.append(nn.Linear(hidden_dim, self.output_size, device=self.device, dtype=torch.float32))
+                            lin1.append(nn.Linear(hidden_dim, self.output_size, device=self.device, dtype=self.dtype))
+                            lin2.append(nn.Linear(hidden_dim, self.output_size, device=self.device, dtype=self.dtype))
                             rnn.append(TensorizedRNN(self.input_size, hidden_dim, self.device, layer=1))
                         else:
                             rnn.append(TensorizedRNN(hidden_dim, hidden_dim, self.device, layer=2))
@@ -161,9 +165,13 @@ class Model(nn.Module):
         self.soft = nn.Softsign()
         
         self.get_num_parameters()
- 
 
-    def forward(self, x, hidden, i, j):
+
+    def forward(self, samples):
+        log_ampl, log_phase = self.log_probabilities(samples)
+        return 0.5*log_ampl, log_phase
+
+    def _forward(self, x, hidden, i, j):
         """
         Passes the input through the network.
         Inputs:
@@ -183,19 +191,19 @@ class Model(nn.Module):
             out = out.contiguous().view(-1, self.hidden_dim)
             hidden = [hidden]
         if self.rnn_type=="Dilated":
-            o, h1 = self.rnn[0][i*self.N_y+j](x[:2], hidden[:2])
+            o, h1   = self.rnn[0][i*self.N_y+j](x[:2], hidden[:2])
             out, h2 = self.rnn[1][i*self.N_y+j](o, hidden[2:4])
             out = out.contiguous().view(-1, self.hidden_dim)
             hidden = [h1, h2]
         return out, hidden
-    
+
     def init_hidden(self, batch_size):
         """
         Generates the hidden state for a given batch size.
         """
         # This method generates the first hidden state of zeros for the forward pass and passes it to the device.
         # A vector of zeros would be  equivalent to a product state.
-        hidden = torch.zeros((batch_size, self.hidden_dim), dtype=torch.float32).to(self.device)
+        hidden = torch.zeros((batch_size, self.hidden_dim), dtype=self.dtype, device=self.device)
         return hidden
     
     def get_num_parameters(self):
@@ -208,17 +216,19 @@ class Model(nn.Module):
                 print(n+": "+str(param.numel()))
                 p += param.numel()
         print("Total number of parameters in the network: "+str(p))
+        self.num_params = p
         return p
+
 
     def enforce_N_total(self, num_up, num_dn, sampled_sites, amplitudes):
         """ Enforces the particle number and magnetization sectors as described in Hibat-Allah 2020 for the magnetization."""
         if self.input_size == 3:
-            bl_up = ((self.N_target+self.sz_tot)//2)*torch.ones((amplitudes.size()[0],1)).to(self.device)
-            bl_dn = ((self.N_target-self.sz_tot)//2)*torch.ones((amplitudes.size()[0],1)).to(self.device)
+            bl_up = ((self.N_target+self.sz_tot*2)//2)*torch.ones((amplitudes.size()[0],1)).to(self.device)
+            bl_dn = ((self.N_target-self.sz_tot*2)//2)*torch.ones((amplitudes.size()[0],1)).to(self.device)
             bl_hole = (self.system_size-self.N_target)*torch.ones((amplitudes.size()[0],1)).to(self.device)
-            ampl_up = torch.heaviside(bl_up-num_up, torch.tensor([0.]).to(self.device))
-            ampl_dn = torch.heaviside(bl_dn-num_dn, torch.tensor([0.]).to(self.device))
-            ampl_hole = torch.heaviside(bl_hole-(sampled_sites-(num_up+num_dn)), torch.tensor([0.]).to(self.device))
+            ampl_up = torch.heaviside(bl_up-num_up, torch.tensor([0.], dtype=self.dtype, device=self.device))
+            ampl_dn = torch.heaviside(bl_dn-num_dn, torch.tensor([0.], dtype=self.dtype, device=self.device))
+            ampl_hole = torch.heaviside(bl_hole-(sampled_sites-(num_up+num_dn)), torch.tensor([0.], dtype=self.dtype, device=self.device))
             ampl = amplitudes * torch.stack([ampl_hole, ampl_dn, ampl_up], axis=1)[:,:,0]
             ampl = torch.nn.functional.normalize(ampl, p=1, eps = 1e-30)
         else:
@@ -235,12 +245,12 @@ class Model(nn.Module):
             hidden.append(hidden_inputs[nl][str(nx+(nl+1)*direction[0])+str(ny)])
             hidden.append(hidden_inputs[nl][str(nx)+str(ny+(nl+1)*direction[1])])
 
-        y, hidden  = self.forward(full_sigma, hidden, nx, ny)
+        y, hidden  = self._forward(full_sigma, hidden, nx, ny)
         # the amplitude is given by a linear layer with a softmax activation
         if not self.weight_sharing or self.rnn_type=="Dilated":
-            ampl = self.lin1[0][nx*self.N_y+ny](y.to(torch.float32))
+            ampl = self.lin1[0][nx*self.N_y+ny](y.to(self.dtype))
         else:
-            ampl = self.lin1(y.to(torch.float32))
+            ampl = self.lin1(y.to(self.dtype))
         ampl = torch.softmax(ampl,dim=1) # amplitude, all elements in a row sum to 1
         if self.sz_tot != None and self.N_target != None:
             ampl = self.enforce_N_total(num_up, num_dn, num_sites, ampl)
@@ -256,7 +266,7 @@ class Model(nn.Module):
         num_dn += sample_dn
         num_sites += 1
         # one hot encode the current sigma to pass it into the GRU at the next time step
-        sigma = nn.functional.one_hot(sample, self.input_size).to(torch.float32).to(self.device)
+        sigma = nn.functional.one_hot(sample, self.input_size).to(self.dtype).to(self.device)
         return sample[:,0], sigma, hidden, num_up, num_dn, num_sites
     
     
@@ -266,7 +276,7 @@ class Model(nn.Module):
         their log probabilities and phases.
         """
         # generate a first input of zeros (sigma and hidden states) to the first GRU cell at t=0
-        sigma       = torch.zeros((num_samples,self.input_size), dtype=torch.float32).to(self.device)
+        sigma       = torch.zeros((num_samples,self.input_size), dtype=self.dtype, device=self.device)
         inputs = {}
         hidden_inputs = []
         for nl in range(self.n_layer):
@@ -277,9 +287,9 @@ class Model(nn.Module):
                     hidden_inputs[nl][str(nx)+str(ny)] = self.init_hidden(num_samples)
         
         samples     = [[[] for ny in range(self.N_y)] for nx in range(self.N_x)]
-        num_up = torch.zeros((num_samples,1), dtype=torch.float32).to(self.device)
-        num_dn = torch.zeros((num_samples,1), dtype=torch.float32).to(self.device)
-        num_sites = torch.zeros((num_samples,1), dtype=torch.float32).to(self.device)
+        num_up = torch.zeros((num_samples,1), dtype=self.dtype, device=self.device)
+        num_dn = torch.zeros((num_samples,1), dtype=self.dtype, device=self.device)
+        num_sites = torch.zeros((num_samples,1), dtype=self.dtype, device=self.device)
         num = 0
         for ny in range(self.N_y):
             if ny % 2 == 0: #go from left to right    
@@ -300,19 +310,20 @@ class Model(nn.Module):
                     num += 1     
         samples = torch.stack([torch.stack(s, axis=1) for s in samples], axis=1).to(self.device)  
         return samples
-    
+
     def _gen_probs(self, nx, ny, direction, samples, inputs, hidden_inputs, num, num_up, num_dn, num_sites):
         # pass the hidden unit and sigma into the GRU cell at t=i 
         # and get the output y (will be used for calculating the 
         # probability) and the next hidden state
-        sample = samples[:,nx,ny].reshape((samples.size()[0],1))
+        num_samples = samples.size()[0]
+        sample = samples[:,nx,ny].reshape((samples.size()[0],1)) #index_select(index_select(samples, 1, torch.tensor([nx], device=self.device)), -1, torch.tensor([ny], device=self.device)).reshape((samples.size()[0],1))
         full_sigma = [inputs[str(nx+direction[0])+str(ny)],inputs[str(nx)+str(ny+direction[1])]]
         hidden = []
         for nl in range(self.n_layer):
             hidden.append(hidden_inputs[nl][str(nx+(nl+1)*direction[0])+str(ny)])
             hidden.append(hidden_inputs[nl][str(nx)+str(ny+(nl+1)*direction[1])])
         
-        y, hidden  = self.forward(full_sigma, hidden, nx, ny)
+        y, hidden  = self._forward(full_sigma, hidden, nx, ny)
         # the amplitude is given by a linear layer with a softmax activation
         if not self.weight_sharing or self.rnn_type=="Dilated":
             ampl = self.lin1[0][nx*self.N_y+ny](y)
@@ -329,16 +340,12 @@ class Model(nn.Module):
         phase = self.soft(phase) 
         # calculate the number of sampled sites and spins
         sample_up = sample.clone()
-        sample_up[sample==1] = 0
-        sample_up[sample==2] = 1
-        num_up += sample_up
+        num_up = torch.where(sample_up==2,num_up+1,num_up)
         sample_dn = sample.clone()
-        sample_dn[sample==2] = 0
-        num_dn += sample_dn
+        num_dn = torch.where(sample_dn==1,num_dn+1,num_dn)
         num_sites += 1
         # one hot encode the current sigma to pass it into the GRU at the next time step
-        sigma = nn.functional.one_hot(sample.reshape((sample.size()[0],1)), self.input_size).to(torch.float32).to(self.device)
-        
+        sigma = self.one_hot(sample, self.input_size).to(self.dtype) #.to(self.device)
         return sigma, ampl, torch.mul(torch.pi,phase), hidden, num_up, num_dn, num_sites
     
     def log_probabilities(self, samples):
@@ -347,11 +354,13 @@ class Model(nn.Module):
         """
         # reshape samples
         samples = samples.to(self.device)
+        if len(samples.size()) == 2:
+            samples = torch.reshape(samples, (1,samples.size(0),samples.size(1)))
         num_samples = samples.size()[0]
         samples = samples.clone().detach()
-        
+
         # generate a first input of zeros (sigma and hidden states) to the first GRU cell at t=0
-        sigma  = torch.zeros((num_samples, self.input_size), dtype=torch.float32).to(self.device)
+        sigma  = torch.zeros((num_samples, self.input_size), dtype=self.dtype, device=self.device)
         inputs = {}
         hidden_inputs = []
         for nl in range(self.n_layer):
@@ -360,9 +369,9 @@ class Model(nn.Module):
                 for nx in range(-1-nl, self.N_x+1+nl):
                     inputs[str(nx)+str(ny)] = sigma
                     hidden_inputs[nl][str(nx)+str(ny)] = self.init_hidden(num_samples)
-        num_up = torch.zeros((num_samples,1), dtype=torch.float32).to(self.device)
-        num_dn = torch.zeros((num_samples,1), dtype=torch.float32).to(self.device)
-        num_sites = torch.zeros((num_samples,1), dtype=torch.float32).to(self.device)
+        num_up = torch.zeros((num_samples,1), dtype=self.dtype, device=self.device)
+        num_dn = torch.zeros((num_samples,1), dtype=self.dtype, device=self.device)
+        num_sites = torch.zeros((num_samples,1), dtype=self.dtype, device=self.device)
         ampl_probs  = [[[] for ny in range(self.N_y)] for nx in range(self.N_x)]
         phase_probs = [[[] for ny in range(self.N_y)] for nx in range(self.N_x)]
         ohs         = [[[] for ny in range(self.N_y)] for nx in range(self.N_x)]
@@ -372,19 +381,19 @@ class Model(nn.Module):
                 for nx in range(self.N_x):
                     direction = [-1,-1]
                     sigma, ampl_probs[nx][ny], phase_probs[nx][ny], hidden, num_up, num_dn, num_sites = self._gen_probs(nx, ny, direction, samples, inputs, hidden_inputs, num, num_up, num_dn, num_sites)
-                    ohs[nx][ny] = sigma
-                    inputs[str(nx)+str(ny)] = sigma
+                    ohs[nx][ny] = sigma.to(self.dtype)
+                    inputs[str(nx)+str(ny)] = sigma.to(self.dtype)
                     hidden_inputs[0][str(nx)+str(ny)] = hidden[0]
-                    if self.n_layer > 1: hidden_inputs[1][str(nx)+str(ny)]  = hidden[1]
+                    if self.n_layer > 1: hidden_inputs[1][str(nx)+str(ny)] = hidden[1]
                     num += 1
             else: #go from right to left
                 for nx in range(self.N_x-1, -1, -1):
                     direction = [1,-1]
                     sigma, ampl_probs[nx][ny], phase_probs[nx][ny], hidden, num_up, num_dn, num_sites = self._gen_probs(nx, ny, direction, samples, inputs, hidden_inputs, num, num_up, num_dn, num_sites)
-                    ohs[nx][ny] = sigma
-                    inputs[str(nx)+str(ny)] = sigma
+                    ohs[nx][ny] = sigma.to(self.dtype)
+                    inputs[str(nx)+str(ny)] = sigma.to(self.dtype)
                     hidden_inputs[0][str(nx)+str(ny)] = hidden[0]
-                    if self.n_layer > 1: hidden_inputs[1][str(nx)+str(ny)]  = hidden[1]
+                    if self.n_layer > 1: hidden_inputs[1][str(nx)+str(ny)] = hidden[1]
                     num += 1
         ampl_probs = torch.cat([torch.stack(a, axis=1) for a in ampl_probs], axis=1) 
         phase_probs = torch.cat([torch.stack(p, axis=1) for p in phase_probs], axis=1) 
@@ -393,4 +402,17 @@ class Model(nn.Module):
         log_probs_ampl = torch.sum(torch.log(torch.sum(torch.torch.multiply(ampl_probs,ohs), axis =2)), axis=1)
         phase = torch.sum((torch.sum(torch.torch.multiply(phase_probs,ohs), axis =2)), axis=1)
         return log_probs_ampl, phase
+
+    def one_hot(self, indices, num_classes):
+        values = torch.arange(num_classes, device=indices.device)
+
+        # Create a tensor of shape (indices.size(0), 1) containing the class indices
+        class_indices = indices.unsqueeze(1)
+
+        # Use the torch.eq function to create a boolean mask indicating
+        # where the class indices match the row indices
+        mask = torch.eq(class_indices, values)
+        # Use the mask to set the appropriate values in the one_hot tensor to 1
+        one_hot = torch.where(mask==True, 1,0)
+        return one_hot
 
